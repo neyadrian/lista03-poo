04c1a008eedaf1d1221232db010b4c9d7b4d7bb8
neyadrian <neyadrian2018@gmail.com>
2026-02-07 14:41:53 -0300

    conta bancária - criando conta

--- src/ContaBancaria/Main.java
+++ src/ContaBancaria/Main.java
@@ -9,7 +9,13 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("-- DIGITE OS DADOS DA CONTA --");
+        System.out.print("Nome: ");
+        String nomeTitular = sc.nextLine();
+        System.out.print("Número da conta: ");
+        int numeroConta = sc.nextInt();
 
+        Conta conta = new Conta(nomeTitular, numeroConta);
 
         sc.close();
     }
