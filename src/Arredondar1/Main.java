0ecd4959747ef3699691423c517fe26669b7e337
neyadrian <neyadrian2018@gmail.com>
2026-02-08 15:05:04 -0300

    arredondar 1

--- src/Arredondar1/Main.java
+++ src/Arredondar1/Main.java
@@ -3,6 +3,19 @@ package Arredondar1;
 public class Main {
     public static void main(String[] args) {
 
+        double num1 = 5.2;
+        double num2 = 5.6;
+        double num3 = -5.8;
+
+        System.out.println("Numeros originais:");
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+
+        System.out.println("Numeros arredondados para o inteiro mais próximo:");
+        System.out.println(Math.round(num1));
+        System.out.println(Math.round(num2));
+        System.out.println(Math.round(num3));
 
     }
 }
