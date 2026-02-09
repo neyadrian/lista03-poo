c6ab349b8d3c1fa73a0b2406cc380af021488dab
neyadrian <neyadrian2018@gmail.com>
2026-02-09 15:44:26 -0300

    raiz quadrada

---
+++ src/RaizQuadrada/Main.java
@@ -0,0 +1,12 @@
+package RaizQuadrada;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double num1 = 900.0, num2 = 30.25;
+
+        System.out.printf("Raiz quadrada de 900: %.2f\n", Math.sqrt(num1));
+        System.out.printf("Raiz quadrada de 30.25: %.2f\n", Math.sqrt(num2));
+
+    }
+}
